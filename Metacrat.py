c4f0cca55be9d7c05eee98d0aad9244b68433ef3
Fabian Granqvist Björk <fabian.granqvistbjo@learnet.se>
2020-10-25 16:55:55 +0100

    added sleep

--- Metacrat.py
+++ Metacrat.py
@@ -2,20 +2,29 @@ import tweepy
 import random
 import os
 from os import environ
+import time
 
-def tweetIt():
+def init():
+    global allTweets, api
     auth = tweepy.OAuthHandler(environ["consumer_key"],environ["consumer_secret"])
     auth.set_access_token(environ["access_key"],environ["access_secret"])
     api = tweepy.API(auth)
     hfileWithTweets = open("BotResults.txt","r")
     allTweets = hfileWithTweets.readlines()
     hfileWithTweets.close()
+
+def tweetIt():
+    global allTweets, api
     tweetIndex = random.randint(0, len(allTweets))
     if (isOdd(tweetIndex)):
         tweetIndex -= 1
     api.update_status(status = (allTweets[tweetIndex]))
+    print("sending tweet " + allTweets[tweetIndex])
 
 def isOdd(my_number):
     return my_number % 2 == 1
 
-tweetIt()
+init()
+while True:
+    tweetIt()
+    time.sleep(3 * 60 * 60)
